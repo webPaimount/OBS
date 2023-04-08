5296dd900d85f5e301c16917750305ee1f7742cd
Richard Stanway <r1ch@r1ch.net>
2023-04-08 18:42:24 +0200

    UI, plugins: Set default bitrate to 10000 kbps
    The default of 2500 kbps was chosen 10 years ago and times have changed.
    Logs and forums posts show that many users of OBS for recording don't
    change their bitrate and end up with a 2.5 mbps recording which looks
    terrible.
    
    Now that service bitrate enforcement exists, this will be automatically
    capped to the maximum bitrate for streaming services, so the only time
    this should result in a problem is if the user's upload speed is the
    limiting factor, hopefully rarer these days with increasing internet
    speeds.

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -1590,7 +1590,7 @@ bool OBSBasic::InitBasicConfigDefaults()
 				  GetDefaultVideoSavePath().c_str());
 	config_set_default_string(basicConfig, "SimpleOutput", "RecFormat2",
 				  DEFAULT_CONTAINER);
-	config_set_default_uint(basicConfig, "SimpleOutput", "VBitrate", 2500);
+	config_set_default_uint(basicConfig, "SimpleOutput", "VBitrate", 10000);
 	config_set_default_uint(basicConfig, "SimpleOutput", "ABitrate", 160);
 	config_set_default_bool(basicConfig, "SimpleOutput", "UseAdvanced",
 				false);
@@ -1634,7 +1634,7 @@ bool OBSBasic::InitBasicConfigDefaults()
 	config_set_default_string(basicConfig, "AdvOut", "FFFilePath",
 				  GetDefaultVideoSavePath().c_str());
 	config_set_default_string(basicConfig, "AdvOut", "FFExtension", "mp4");
-	config_set_default_uint(basicConfig, "AdvOut", "FFVBitrate", 2500);
+	config_set_default_uint(basicConfig, "AdvOut", "FFVBitrate", 10000);
 	config_set_default_uint(basicConfig, "AdvOut", "FFVGOPSize", 250);
 	config_set_default_bool(basicConfig, "AdvOut", "FFUseRescale", false);
 	config_set_default_bool(basicConfig, "AdvOut", "FFIgnoreCompat", false);
