40a40e080ed671c1f7f297e6b6e8a821a1c32f3f
cg2121 <claytong1214@gmail.com>
2023-12-09 20:53:34 -0600

    UI: Add HotkeyBlocker class
    This adds a HotkeyBlocker class that is similar to SignalBlocker.
    It is used when selection dialogs are opened and because they are
    modal, hotkeys shouldn't be able to be used.

--- UI/properties-view.cpp
+++ UI/properties-view.cpp
@@ -1898,6 +1898,7 @@ bool WidgetInfo::ColorChangedInternal(const char *setting, bool supportAlpha)
 	options |= QColorDialog::DontUseNativeDialog;
 #endif
 
+	HotkeyBlocker hb;
 	color = QColorDialog::getColor(color, view, QT_UTF8(desc), options);
 
 #ifdef __APPLE__
@@ -1951,6 +1952,8 @@ bool WidgetInfo::FontChanged(const char *setting)
 #ifndef _WIN32
 	options = QFontDialog::DontUseNativeDialog;
 #endif
+
+	HotkeyBlocker hb;
 
 	if (!font_obj) {
 		QFont initial;
