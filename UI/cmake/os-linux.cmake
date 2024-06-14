kenchan/obs-studio

UI: Update formatting of CMake files

UI/cmake/os-linux.cmake
@@ -14,7 +14,8 @@ if(NOT DEFINED APPDATA_RELEASE_DATE)
       COMMAND git log --tags -1 --pretty=%cd --date=short
       OUTPUT_VARIABLE APPDATA_RELEASE_DATE
       WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
-      OUTPUT_STRIP_TRAILING_WHITESPACE)
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
   elseif(EXISTS "${CMAKE_SOURCE_DIR}/cmake/.CMakeBuildNumber")
     file(TIMESTAMP "${CMAKE_SOURCE_DIR}/cmake/.CMakeBuildNumber" APPDATA_RELEASE_DATE "%Y-%m-%d")
   else()
@@ -24,27 +25,33 @@ endif()
 
 configure_file(cmake/linux/com.obsproject.Studio.appdata.xml.in com.obsproject.Studio.appdata.xml)
 
-install(FILES "${CMAKE_CURRENT_BINARY_DIR}/com.obsproject.Studio.appdata.xml"
-        DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/metainfo")
+install(
+  FILES "${CMAKE_CURRENT_BINARY_DIR}/com.obsproject.Studio.appdata.xml"
+  DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/metainfo"
+)
 
 install(FILES cmake/linux/com.obsproject.Studio.desktop DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/applications")
 
 install(
   FILES cmake/linux/icons/obs-logo-128.png
   DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/icons/hicolor/128x128/apps"
-  RENAME com.obsproject.Studio.png)
+  RENAME com.obsproject.Studio.png
+)
 
 install(
   FILES cmake/linux/icons/obs-logo-256.png
   DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/icons/hicolor/256x256/apps"
-  RENAME com.obsproject.Studio.png)
+  RENAME com.obsproject.Studio.png
+)
 
 install(
   FILES cmake/linux/icons/obs-logo-512.png
   DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/icons/hicolor/512x512/apps"
-  RENAME com.obsproject.Studio.png)
+  RENAME com.obsproject.Studio.png
+)
 
 install(
   FILES cmake/linux/icons/obs-logo-scalable.svg
   DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/icons/hicolor/scalable/apps"
-  RENAME com.obsproject.Studio.svg)
+  RENAME com.obsproject.Studio.svg
+)

UI/cmake/ui-elements.cmake
@@ -3,25 +3,26 @@ add_library(OBS::ui-support ALIAS obs-ui-support)
 
 target_sources(
   obs-ui-support
-  INTERFACE # cmake-format: sortable
-            clickable-label.hpp
-            double-slider.cpp
-            double-slider.hpp
-            horizontal-scroll-area.cpp
-            horizontal-scroll-area.hpp
-            plain-text-edit.cpp
-            plain-text-edit.hpp
-            properties-view.cpp
-            properties-view.hpp
-            properties-view.moc.hpp
-            qt-wrappers.cpp
-            qt-wrappers.hpp
-            slider-ignorewheel.cpp
-            slider-ignorewheel.hpp
-            spinbox-ignorewheel.cpp
-            spinbox-ignorewheel.hpp
-            vertical-scroll-area.cpp
-            vertical-scroll-area.hpp)
+  INTERFACE
+    clickable-label.hpp
+    double-slider.cpp
+    double-slider.hpp
+    horizontal-scroll-area.cpp
+    horizontal-scroll-area.hpp
+    plain-text-edit.cpp
+    plain-text-edit.hpp
+    properties-view.cpp
+    properties-view.hpp
+    properties-view.moc.hpp
+    qt-wrappers.cpp
+    qt-wrappers.hpp
+    slider-ignorewheel.cpp
+    slider-ignorewheel.hpp
+    spinbox-ignorewheel.cpp
+    spinbox-ignorewheel.hpp
+    vertical-scroll-area.cpp
+    vertical-scroll-area.hpp
+)
 
 target_include_directories(obs-ui-support INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}")
 
@@ -29,49 +30,50 @@ target_link_libraries(obs-studio PRIVATE OBS::ui-support)
 
 target_sources(
   obs-studio
-  PRIVATE # cmake-format: sortable
-          adv-audio-control.cpp
-          adv-audio-control.hpp
-          audio-encoders.cpp
-          audio-encoders.hpp
-          balance-slider.hpp
-          context-bar-controls.cpp
-          context-bar-controls.hpp
-          focus-list.cpp
-          focus-list.hpp
-          hotkey-edit.cpp
-          hotkey-edit.hpp
-          item-widget-helpers.cpp
-          item-widget-helpers.hpp
-          lineedit-autoresize.cpp
-          lineedit-autoresize.hpp
-          log-viewer.cpp
-          log-viewer.hpp
-          media-controls.cpp
-          media-controls.hpp
-          media-slider.cpp
-          media-slider.hpp
-          menu-button.cpp
-          menu-button.hpp
-          mute-checkbox.hpp
-          record-button.cpp
-          record-button.hpp
-          remote-text.cpp
-          remote-text.hpp
-          scene-tree.cpp
-          scene-tree.hpp
-          screenshot-obj.hpp
-          slider-absoluteset-style.cpp
-          slider-absoluteset-style.hpp
-          source-label.cpp
-          source-label.hpp
-          source-tree.cpp
-          source-tree.hpp
-          undo-stack-obs.cpp
-          undo-stack-obs.hpp
-          url-push-button.cpp
-          url-push-button.hpp
-          visibility-item-widget.cpp
-          visibility-item-widget.hpp
-          volume-control.cpp
-          volume-control.hpp)
+  PRIVATE
+    adv-audio-control.cpp
+    adv-audio-control.hpp
+    audio-encoders.cpp
+    audio-encoders.hpp
+    balance-slider.hpp
+    context-bar-controls.cpp
+    context-bar-controls.hpp
+    focus-list.cpp
+    focus-list.hpp
+    hotkey-edit.cpp
+    hotkey-edit.hpp
+    item-widget-helpers.cpp
+    item-widget-helpers.hpp
+    lineedit-autoresize.cpp
+    lineedit-autoresize.hpp
+    log-viewer.cpp
+    log-viewer.hpp
+    media-controls.cpp
+    media-controls.hpp
+    media-slider.cpp
+    media-slider.hpp
+    menu-button.cpp
+    menu-button.hpp
+    mute-checkbox.hpp
+    record-button.cpp
+    record-button.hpp
+    remote-text.cpp
+    remote-text.hpp
+    scene-tree.cpp
+    scene-tree.hpp
+    screenshot-obj.hpp
+    slider-absoluteset-style.cpp
+    slider-absoluteset-style.hpp
+    source-label.cpp
+    source-label.hpp
+    source-tree.cpp
+    source-tree.hpp
+    undo-stack-obs.cpp
+    undo-stack-obs.hpp
+    url-push-button.cpp
+    url-push-button.hpp
+    visibility-item-widget.cpp
+    visibility-item-widget.hpp
+    volume-control.cpp
+    volume-control.hpp
+)

webPaimount/OBS

UI/cmake/os-linux.cmake
@@ -16,7 +16,8 @@ if(NOT DEFINED APPDATA_RELEASE_DATE)
       COMMAND git log --tags -1 --pretty=%cd --date=short
       OUTPUT_VARIABLE APPDATA_RELEASE_DATE
       WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
-      OUTPUT_STRIP_TRAILING_WHITESPACE)
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
   elseif(EXISTS "${CMAKE_SOURCE_DIR}/cmake/.CMakeBuildNumber")
     file(TIMESTAMP "${CMAKE_SOURCE_DIR}/cmake/.CMakeBuildNumber" APPDATA_RELEASE_DATE "%Y-%m-%d")
   else()
@@ -30,7 +31,8 @@ if(NOT DEFINED GIT_HASH)
       COMMAND git rev-parse HEAD
       OUTPUT_VARIABLE GIT_HASH
       WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
-      OUTPUT_STRIP_TRAILING_WHITESPACE)
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
   else()
     set(GIT_HASH "master")
   endif()
@@ -38,27 +40,33 @@ endif()
 
 configure_file(cmake/linux/com.obsproject.Studio.metainfo.xml.in com.obsproject.Studio.metainfo.xml)
 
-install(FILES "${CMAKE_CURRENT_BINARY_DIR}/com.obsproject.Studio.metainfo.xml"
-        DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/metainfo")
+install(
+  FILES "${CMAKE_CURRENT_BINARY_DIR}/com.obsproject.Studio.metainfo.xml"
+  DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/metainfo"
+)
 
 install(FILES cmake/linux/com.obsproject.Studio.desktop DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/applications")
 
 install(
   FILES cmake/linux/icons/obs-logo-128.png
   DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/icons/hicolor/128x128/apps"
-  RENAME com.obsproject.Studio.png)
+  RENAME com.obsproject.Studio.png
+)
 
 install(
   FILES cmake/linux/icons/obs-logo-256.png
   DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/icons/hicolor/256x256/apps"
-  RENAME com.obsproject.Studio.png)
+  RENAME com.obsproject.Studio.png
+)
 
 install(
   FILES cmake/linux/icons/obs-logo-512.png
   DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/icons/hicolor/512x512/apps"
-  RENAME com.obsproject.Studio.png)
+  RENAME com.obsproject.Studio.png
+)
 
 install(
   FILES cmake/linux/icons/obs-logo-scalable.svg
   DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/icons/hicolor/scalable/apps"
-  RENAME com.obsproject.Studio.svg)
+  RENAME com.obsproject.Studio.svg
+)

UI/cmake/ui-elements.cmake
@@ -3,25 +3,26 @@ add_library(OBS::ui-support ALIAS obs-ui-support)
 
 target_sources(
   obs-ui-support
-  INTERFACE # cmake-format: sortable
-            clickable-label.hpp
-            double-slider.cpp
-            double-slider.hpp
-            horizontal-scroll-area.cpp
-            horizontal-scroll-area.hpp
-            plain-text-edit.cpp
-            plain-text-edit.hpp
-            properties-view.cpp
-            properties-view.hpp
-            properties-view.moc.hpp
-            qt-wrappers.cpp
-            qt-wrappers.hpp
-            slider-ignorewheel.cpp
-            slider-ignorewheel.hpp
-            spinbox-ignorewheel.cpp
-            spinbox-ignorewheel.hpp
-            vertical-scroll-area.cpp
-            vertical-scroll-area.hpp)
+  INTERFACE
+    clickable-label.hpp
+    double-slider.cpp
+    double-slider.hpp
+    horizontal-scroll-area.cpp
+    horizontal-scroll-area.hpp
+    plain-text-edit.cpp
+    plain-text-edit.hpp
+    properties-view.cpp
+    properties-view.hpp
+    properties-view.moc.hpp
+    qt-wrappers.cpp
+    qt-wrappers.hpp
+    slider-ignorewheel.cpp
+    slider-ignorewheel.hpp
+    spinbox-ignorewheel.cpp
+    spinbox-ignorewheel.hpp
+    vertical-scroll-area.cpp
+    vertical-scroll-area.hpp
+)
 
 target_include_directories(obs-ui-support INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}")
 target_compile_options(obs-ui-support INTERFACE $<$<PLATFORM_ID:Linux>:-Wno-error=enum-conversion>)
@@ -30,48 +31,49 @@ target_link_libraries(obs-studio PRIVATE OBS::ui-support)
 
 target_sources(
   obs-studio
-  PRIVATE # cmake-format: sortable
-          absolute-slider.cpp
-          absolute-slider.hpp
-          adv-audio-control.cpp
-          adv-audio-control.hpp
-          audio-encoders.cpp
-          audio-encoders.hpp
-          balance-slider.hpp
-          basic-controls.cpp
-          basic-controls.hpp
-          context-bar-controls.cpp
-          context-bar-controls.hpp
-          focus-list.cpp
-          focus-list.hpp
-          hotkey-edit.cpp
-          hotkey-edit.hpp
-          item-widget-helpers.cpp
-          item-widget-helpers.hpp
-          lineedit-autoresize.cpp
-          lineedit-autoresize.hpp
-          log-viewer.cpp
-          log-viewer.hpp
-          media-controls.cpp
-          media-controls.hpp
-          menu-button.cpp
-          menu-button.hpp
-          mute-checkbox.hpp
-          noncheckable-button.hpp
-          remote-text.cpp
-          remote-text.hpp
-          scene-tree.cpp
-          scene-tree.hpp
-          screenshot-obj.hpp
-          source-label.cpp
-          source-label.hpp
-          source-tree.cpp
-          source-tree.hpp
-          undo-stack-obs.cpp
-          undo-stack-obs.hpp
-          url-push-button.cpp
-          url-push-button.hpp
-          visibility-item-widget.cpp
-          visibility-item-widget.hpp
-          volume-control.cpp
-          volume-control.hpp)
+  PRIVATE
+    absolute-slider.cpp
+    absolute-slider.hpp
+    adv-audio-control.cpp
+    adv-audio-control.hpp
+    audio-encoders.cpp
+    audio-encoders.hpp
+    balance-slider.hpp
+    basic-controls.cpp
+    basic-controls.hpp
+    context-bar-controls.cpp
+    context-bar-controls.hpp
+    focus-list.cpp
+    focus-list.hpp
+    hotkey-edit.cpp
+    hotkey-edit.hpp
+    item-widget-helpers.cpp
+    item-widget-helpers.hpp
+    lineedit-autoresize.cpp
+    lineedit-autoresize.hpp
+    log-viewer.cpp
+    log-viewer.hpp
+    media-controls.cpp
+    media-controls.hpp
+    menu-button.cpp
+    menu-button.hpp
+    mute-checkbox.hpp
+    noncheckable-button.hpp
+    remote-text.cpp
+    remote-text.hpp
+    scene-tree.cpp
+    scene-tree.hpp
+    screenshot-obj.hpp
+    source-label.cpp
+    source-label.hpp
+    source-tree.cpp
+    source-tree.hpp
+    undo-stack-obs.cpp
+    undo-stack-obs.hpp
+    url-push-button.cpp
+    url-push-button.hpp
+    visibility-item-widget.cpp
+    visibility-item-widget.hpp
+    volume-control.cpp
+    volume-control.hpp
+)